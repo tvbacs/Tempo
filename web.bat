a981a6c5be7bc6e7908cb5053a32a68655484431
tvbacs <vbax0502@gmail.com>
2026-08-31 10:01:00 +0700

    chore: add web.bat for easy one-click web client launch

---
+++ web.bat
@@ -0,0 +1,8 @@
+@echo off
+title Tempo Music Web Client
+cd /d "%~dp0Web\Tempo"
+echo ==================================================
+echo   Starting Tempo Web Player (React + Vite)
+echo ==================================================
+npm run dev
+pause
